d991a8b28a31d8a524d62a7b57cc1c7d4c716f2d
Eclipsexc <162338329+Eclipsexc@users.noreply.github.com>
2026-02-18 07:21:31 +0200

    check reading dataset is valid

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ if __name__ == "__main__":
         .schema(crime_schema)
         .csv(path)
     )
+
+    if df.count() == 0:
+        raise ValueError("DataFrame is empty")
+
+    if "ID" not in df.columns:
+        raise ValueError("ID column missing")
+
+    if dict(df.dtypes)["Date"] != "timestamp":
+        raise TypeError("Date column is not timestamp")
+
+    print("DataFrame validation passed.")
