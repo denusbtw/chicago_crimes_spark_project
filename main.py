7d5f7f59e6f1199df83c371fc58a77e10d6f5512
denusbtw <124472635+denusbtw@users.noreply.github.com>
2026-02-18 07:23:14 +0200

    create module and load_crime_data

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 from pyspark.sql import SparkSession
-from schemas import crime_schema
+
+from extractor import load_crime_data, validate_dataframe
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("ChicagoCrimes").getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    path = "data/chicago_crimes.csv"
-    df = (
-        spark.read
-        .option("header", True)
-        .option("sep", ";")
-        .option("quote", '"')
-        .option("timestampFormat", "MM/dd/yyyy hh:mm:ss a")
-        .schema(crime_schema)
-        .csv(path)
-    )
+    df = load_crime_data(spark, "data/chicago_crimes.csv")
 
-    if df.count() == 0:
-        raise ValueError("DataFrame is empty")
-
-    if "ID" not in df.columns:
-        raise ValueError("ID column missing")
-
-    if dict(df.dtypes)["Date"] != "timestamp":
-        raise TypeError("Date column is not timestamp")
-
-    print("DataFrame validation passed.")
+    validate_dataframe(df)
